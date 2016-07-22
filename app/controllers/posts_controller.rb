aa7ea95fb673bdfd9c90712bb0bda633fe3b69be
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-07-22 16:55:18 -0400

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,15 @@
 class PostsController < ApplicationController
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @post = params
+    redirect_to new_post_path(post: @post)
+  end
+
+
+
+
 end
