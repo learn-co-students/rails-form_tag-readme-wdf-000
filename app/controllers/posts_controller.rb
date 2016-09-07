1f33aac753c0ddbd4ecc5d8b6dcca429639ad55a
Flatiron School <github email address>
2016-09-07 14:04:15 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,14 @@
 class PostsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    # @post = Post.create(title: params[:title], description: params[:description])
+    # @post = Post.create(params)
+    @post = params
+    redirect_to new_post_path(post: @post)
+  end
+
+
 end
