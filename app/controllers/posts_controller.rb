965e10cfd7f4316dd3558e98a7319782e3366d48
parm530 <parmsahadeo530@gmail.com>
2016-09-23 17:33:56 -0400

    DONE

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,14 @@
 class PostsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+  def index 
+  end
+
 end
