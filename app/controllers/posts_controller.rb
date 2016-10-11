46242a1a0ea58a43a4788e2e560e8d1ceb16135a
Esmery Corniel <esmerycornielle@gmail.com>
2016-10-11 10:29:48 -0400

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,14 @@
 class PostsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    Post.create(title: params[:title], description: params[:description])
+    redirect_to posts_path
+  end
+
+  def index
+  end
+
 end
